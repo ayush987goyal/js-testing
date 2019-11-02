193cb3b10b265f8417cf8b4f9fed74db8b484b33
Ayush Goyal <ayush987goyal@gmail.com>
2019-11-02 09:26:38 +0530

    Add identity obj proxy for  css names

--- jest-cypress-react-babel-webpack/src/shared/__tests__/auto-scaling-text.js
+++ jest-cypress-react-babel-webpack/src/shared/__tests__/auto-scaling-text.js
@@ -3,5 +3,6 @@ import {render} from '@testing-library/react'
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
-  render(<AutoScalingText />)
+  const {container} = render(<AutoScalingText />)
+  console.log(container.innerHTML)
 })
