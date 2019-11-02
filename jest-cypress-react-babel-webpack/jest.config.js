22c19211e8061f99c5ee850ced42c659a4ef30c3
Ayush Goyal <ayush987goyal@gmail.com>
2019-11-02 11:05:15 +0530

    Add setup-tests

--- jest-cypress-react-babel-webpack/jest.config.js
+++ jest-cypress-react-babel-webpack/jest.config.js
@@ -4,4 +4,6 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  // after jest is loaded
+  setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
 }
