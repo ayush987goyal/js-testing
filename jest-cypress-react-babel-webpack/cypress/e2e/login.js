ff6aa56c363a6f7ff8fa0fcddc4e9a7d33705093
Ayush Goyal <ayush987goyal@gmail.com>
2019-11-03 11:33:30 +0530

    add login with custom command to create

---
+++ jest-cypress-react-babel-webpack/cypress/e2e/login.js
@@ -0,0 +1,23 @@
+describe('login', () => {
+  it('should login an existing user', () => {
+    cy.createUser().then(user => {
+      cy.visit('/')
+        .findByText(/login/i)
+        .click()
+        .findByLabelText(/username/i)
+        .type(user.username)
+        .findByLabelText(/password/i)
+        .type(user.password)
+        .findByText(/submit/i)
+        .click()
+
+        .url()
+        .should('eq', `${Cypress.config().baseUrl}/`)
+        .window()
+        .its('localStorage.token')
+        .should('be.a', 'string')
+        .findByTestId('username-display')
+        .should('have.text', user.username)
+    })
+  })
+})
