ae31fe59e9d019415d2180679afb703ad8df1589
Ayush Goyal <ayush987goyal@gmail.com>
2019-12-04 22:49:04 +0530

    Add test for pure func

--- testing-node-apps/src/utils/__tests__/auth.exercise.js
+++ testing-node-apps/src/utils/__tests__/auth.exercise.js
@@ -1,21 +1,15 @@
 // Testing Pure Functions
+import {isPasswordAllowed} from '../auth'
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
+test('isPasswordAllowed returns true for valid passwords', () => {
+  expect(isPasswordAllowed('!aBc123')).toBe(true)
+})
 
-// 🐨 import the function that we're testing
-// 💰 import {isPasswordAllowed} from '../auth'
-
-// 🐨 write tests for valid and invalid passwords
-// 💰 here are some you can use:
-//
-// valid:
-// - !aBc123
-//
-// invalid:
-// - a2c! // too short
-// - 123456! // no alphabet characters
-// - ABCdef! // no numbers
-// - abc123! // no uppercase letters
-// - ABC123! // no lowercase letters
-// - ABCdef123 // no non-alphanumeric characters
+test('isPasswordAllowed returns false for invalid passwords', () => {
+  expect(isPasswordAllowed('a2c!')).toBe(false)
+  expect(isPasswordAllowed('123@!2')).toBe(false)
+  expect(isPasswordAllowed('aBce@!')).toBe(false)
+  expect(isPasswordAllowed('abc123@')).toBe(false)
+  expect(isPasswordAllowed('ABC123!')).toBe(false)
+  expect(isPasswordAllowed('abcAv23')).toBe(false)
+})
